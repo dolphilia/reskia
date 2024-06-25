43471f41f5e28857599050c11a7c95f89968c548
dolphilia <dolphilia@outlook.com>
2024-06-25 23:44:08 +0900

    update

--- skia/static/static_sk_color.cpp
+++ skia/static/static_sk_color.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "static_sk_color.h"
-
+/*
 static std::set<int> static_sk_color_available_keys;
 static std::map<int , SkSpan<SkColor>> static_sk_color;
 static int static_sk_color_index = 0;
@@ -36,4 +36,46 @@ SkColor static_sk_color_get_value(int key, int index) {
 
 void static_color_set(int key, SkSpan<SkColor> value) {
     static_sk_color[key] = value;
+}*/
+//
+// Created by dolphilia on 2024/01/12.
+//
+
+#include "static_sk_color.h"
+
+struct color_t{
+    int r;
+    int g;
+    int b;
+    int a;
+};
+
+static std::set<int> color_available_keys;
+static std::map<int, color_t> colors;
+static int color_index = 0;
+
+int color_make(color_t value) {
+    int key;
+    if (!color_available_keys.empty()) {
+        auto it = color_available_keys.begin();
+        key = *it;
+        color_available_keys.erase(it);
+    } else {
+        key = color_index++;
+    }
+    colors[key] = value;
+    return key;
+}
+
+void color_delete(int key) {
+    colors.erase(key);
+    color_available_keys.insert(key);
+}
+
+color_t color_get(int key) {
+    return colors[key];
+}
+
+void color_set(int key, color_t value) {
+    colors[key] = value;
 }
