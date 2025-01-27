0bc8ac9c1460d2f375b157c076edcdfde1c874f4
dolphilia <dolphilia@outlook.com>
2025-01-27 17:07:10 +0900

    update

--- skia/binding/sk_yuva_pixmaps.cpp
+++ skia/binding/sk_yuva_pixmaps.cpp
@@ -4,6 +4,12 @@
 
 #include "sk_yuva_pixmaps.h"
 
+#include "include/core/SkYUVAPixmaps.h"
+
+#include "../static/static_sk_yuva_pixmap_info.h"
+#include "../static/static_sk_yuva_pixmaps.h"
+#include "../static/static_sk_data.h"
+
 #include "../static/static_sk_yuva_pixmaps-internal.h"
 #include "../static/static_sk_yuva_pixmap_info-internal.h"
 #include "../static/static_sk_data-internal.h"
@@ -13,78 +19,78 @@ extern "C" {
 // SkYUVAPixmaps & operator=(SkYUVAPixmaps &&that)
 // SkYUVAPixmaps & operator=(const SkYUVAPixmaps &that)
 
-SkYUVAPixmaps *SkYUVAPixmaps_new() {
+void *SkYUVAPixmaps_new() {
     return new SkYUVAPixmaps();
 }
 
-SkYUVAPixmaps *SkYUVAPixmaps_new_2(const SkYUVAPixmaps *pixmaps) {
-    return new SkYUVAPixmaps(*pixmaps);
+void *SkYUVAPixmaps_new_2(const void *pixmaps) {
+    return new SkYUVAPixmaps(* static_cast<const SkYUVAPixmaps *>(pixmaps));
 }
 
-void SkYUVAPixmaps_delete(SkYUVAPixmaps *yuva_pixmaps) {
-    delete yuva_pixmaps;
+void SkYUVAPixmaps_delete(void *yuva_pixmaps) {
+    delete static_cast<SkYUVAPixmaps *>(yuva_pixmaps);
 }
 
-bool SkYUVAPixmaps_isValid(SkYUVAPixmaps *yuva_pixmaps) {
-    return yuva_pixmaps->isValid();
+bool SkYUVAPixmaps_isValid(void *yuva_pixmaps) {
+    return static_cast<SkYUVAPixmaps *>(yuva_pixmaps)->isValid();
 }
 
-const SkYUVAInfo * SkYUVAPixmaps_yuvaInfo(SkYUVAPixmaps *yuva_pixmaps) {
-    return &yuva_pixmaps->yuvaInfo();
+const void * SkYUVAPixmaps_yuvaInfo(void *yuva_pixmaps) {
+    return &static_cast<SkYUVAPixmaps *>(yuva_pixmaps)->yuvaInfo();
 }
 
-SkYUVAPixmaps::DataType SkYUVAPixmaps_dataType(SkYUVAPixmaps *yuva_pixmaps) {
-    return yuva_pixmaps->dataType();
+int SkYUVAPixmaps_dataType(void *yuva_pixmaps) {
+    return static_cast<int>(static_cast<SkYUVAPixmaps *>(yuva_pixmaps)->dataType());
 }
 
-sk_yuva_pixmap_info_t SkYUVAPixmaps_pixmapsInfo(SkYUVAPixmaps *yuva_pixmaps) {
-    return static_sk_yuva_pixmap_info_make(yuva_pixmaps->pixmapsInfo());
+sk_yuva_pixmap_info_t SkYUVAPixmaps_pixmapsInfo(void *yuva_pixmaps) {
+    return static_sk_yuva_pixmap_info_make(static_cast<SkYUVAPixmaps *>(yuva_pixmaps)->pixmapsInfo());
 }
 
-int SkYUVAPixmaps_numPlanes(SkYUVAPixmaps *yuva_pixmaps) {
-    return yuva_pixmaps->numPlanes();
+int SkYUVAPixmaps_numPlanes(void *yuva_pixmaps) {
+    return static_cast<SkYUVAPixmaps *>(yuva_pixmaps)->numPlanes();
 }
 
-const std::array<SkPixmap, SkYUVAPixmaps::kMaxPlanes> * SkYUVAPixmaps_planes(SkYUVAPixmaps *yuva_pixmaps) {
-    return &yuva_pixmaps->planes();
+const void * SkYUVAPixmaps_planes(void *yuva_pixmaps) {
+    return &static_cast<SkYUVAPixmaps *>(yuva_pixmaps)->planes();
 }
 
-const SkPixmap * SkYUVAPixmaps_plane(SkYUVAPixmaps *yuva_pixmaps, int i) {
-    return &yuva_pixmaps->plane(i);
+const void * SkYUVAPixmaps_plane(void *yuva_pixmaps, int i) {
+    return &static_cast<SkYUVAPixmaps *>(yuva_pixmaps)->plane(i);
 }
 
 //SkYUVAInfo::YUVALocations SkYUVAPixmaps_toYUVALocations(SkYUVAPixmaps *yuva_pixmaps) {
 //    return yuva_pixmaps->toYUVALocations();
 //}
 
-bool SkYUVAPixmaps_ownsStorage(SkYUVAPixmaps *yuva_pixmaps) {
-    return yuva_pixmaps->ownsStorage();
+bool SkYUVAPixmaps_ownsStorage(void *yuva_pixmaps) {
+    return static_cast<SkYUVAPixmaps *>(yuva_pixmaps)->ownsStorage();
 }
 
 // static
 
-SkColorType SkYUVAPixmaps_RecommendedRGBAColorType(SkYUVAPixmaps::DataType type) {
-    return SkYUVAPixmaps::RecommendedRGBAColorType(type);
+int SkYUVAPixmaps_RecommendedRGBAColorType(int type) {
+    return SkYUVAPixmaps::RecommendedRGBAColorType(static_cast<SkYUVAPixmaps::DataType>(type));
 }
 
-sk_yuva_pixmaps_t SkYUVAPixmaps_Allocate(const SkYUVAPixmapInfo *yuvaPixmapInfo) {
-    return static_sk_yuva_pixmaps_make(SkYUVAPixmaps::Allocate(*yuvaPixmapInfo));
+sk_yuva_pixmaps_t SkYUVAPixmaps_Allocate(const void *yuvaPixmapInfo) {
+    return static_sk_yuva_pixmaps_make(SkYUVAPixmaps::Allocate(* static_cast<const SkYUVAPixmapInfo *>(yuvaPixmapInfo)));
 }
 
-sk_yuva_pixmaps_t SkYUVAPixmaps_FromData(const SkYUVAPixmapInfo *yuvaPixmapInfo, sk_data_t data) {
-    return static_sk_yuva_pixmaps_make(SkYUVAPixmaps::FromData(*yuvaPixmapInfo, static_sk_data_move(data)));
+sk_yuva_pixmaps_t SkYUVAPixmaps_FromData(const void *yuvaPixmapInfo, sk_data_t data) {
+    return static_sk_yuva_pixmaps_make(SkYUVAPixmaps::FromData(* static_cast<const SkYUVAPixmapInfo *>(yuvaPixmapInfo), static_sk_data_move(data)));
 }
 
-sk_yuva_pixmaps_t SkYUVAPixmaps_MakeCopy(const SkYUVAPixmaps *src) {
-    return static_sk_yuva_pixmaps_make(SkYUVAPixmaps::MakeCopy(*src));
+sk_yuva_pixmaps_t SkYUVAPixmaps_MakeCopy(const void *src) {
+    return static_sk_yuva_pixmaps_make(SkYUVAPixmaps::MakeCopy(* static_cast<const SkYUVAPixmaps *>(src)));
 }
 
-sk_yuva_pixmaps_t SkYUVAPixmaps_FromExternalMemory(const SkYUVAPixmapInfo *yuvaPixmapInfo, void *memory) {
-    return static_sk_yuva_pixmaps_make(SkYUVAPixmaps::FromExternalMemory(*yuvaPixmapInfo, memory));
+sk_yuva_pixmaps_t SkYUVAPixmaps_FromExternalMemory(const void *yuvaPixmapInfo, void *memory) {
+    return static_sk_yuva_pixmaps_make(SkYUVAPixmaps::FromExternalMemory(* static_cast<const SkYUVAPixmapInfo *>(yuvaPixmapInfo), memory));
 }
 
-sk_yuva_pixmaps_t SkYUVAPixmaps_FromExternalPixmaps(const SkYUVAInfo *yuvaInfo, const SkPixmap pixmaps[4]) {
-    return static_sk_yuva_pixmaps_make(SkYUVAPixmaps::FromExternalPixmaps(*yuvaInfo, pixmaps));
+sk_yuva_pixmaps_t SkYUVAPixmaps_FromExternalPixmaps(const void *yuvaInfo, const void * pixmaps) {
+    return static_sk_yuva_pixmaps_make(SkYUVAPixmaps::FromExternalPixmaps(* static_cast<const SkYUVAInfo *>(yuvaInfo), static_cast<const SkPixmap *>(pixmaps)));
 }
 
 }
